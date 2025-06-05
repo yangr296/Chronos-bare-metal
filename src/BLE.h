c9ba5e84090cb33877e35e231e7df20977c793e4
yangr296 <tyang296@wisc.edu>
2025-06-05 14:28:29 -0500

    base spi/timer and BLE integration complete

---
+++ src/BLE.h
@@ -0,0 +1,88 @@
+#ifndef BLE_H
+#define BLE_H
+
+#include <zephyr/types.h>
+#include <zephyr/device.h>
+
+#define LOG_MODULE_NAME peripheral_uart
+
+#define STACKSIZE CONFIG_BT_NUS_THREAD_STACK_SIZE
+#define PRIORITY 7
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN	(sizeof(DEVICE_NAME) - 1)
+
+#define RUN_STATUS_LED DK_LED1
+#define RUN_LED_BLINK_INTERVAL 1000
+
+#define CON_STATUS_LED DK_LED2
+
+#define KEY_PASSKEY_ACCEPT DK_BTN1_MSK
+#define KEY_PASSKEY_REJECT DK_BTN2_MSK
+
+#define UART_BUF_SIZE CONFIG_BT_NUS_UART_BUFFER_SIZE
+#define UART_WAIT_FOR_BUF_DELAY K_MSEC(50)
+#define UART_WAIT_FOR_RX CONFIG_BT_NUS_UART_RX_WAIT_TIME
+
+#ifdef CONFIG_UART_ASYNC_ADAPTER
+UART_ASYNC_ADAPTER_INST_DEFINE(async_adapter);
+#else
+#define async_adapter NULL
+#endif // CONFIG_UART_ASYNC_ADAPTER
+
+extern struct k_sem ble_init_ok;
+extern struct k_work_delayable uart_work;
+extern const struct device *uart;
+extern struct k_work adv_work;
+extern struct bt_conn *current_conn;
+extern struct bt_conn *auth_conn;
+extern const struct bt_data ad[];
+extern const struct bt_data sd[];
+extern const size_t ad_len;
+extern const size_t sd_len;
+struct uart_data_t {
+    void *fifo_reserved;
+    uint8_t data[UART_BUF_SIZE];
+    uint16_t len;
+};
+
+void uart_work_handler(struct k_work *item);
+bool uart_test_async_api(const struct device *dev);
+void adv_work_handler(struct k_work *work);
+void advertising_start(void);
+void connected(struct bt_conn *conn, uint8_t err);
+void disconnected(struct bt_conn *conn, uint8_t reason);
+void recycled_cb(void);
+void configure_gpio(void);
+void error(void);
+void bt_receive_cb(struct bt_conn *conn, const uint8_t *const data,
+			  uint16_t len);
+void uart_cb(const struct device *dev, struct uart_event *evt, void *user_data);
+int uart_init(void);
+void ble_write_thread(void);
+
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+void security_changed(struct bt_conn *conn, bt_security_t level,
+                 enum bt_security_err err);
+#endif /* CONFIG_BT_NUS_SECURITY_ENABLED */
+
+#if defined(CONFIG_BT_NUS_SECURITY_ENABLED)
+void auth_passkey_display(struct bt_conn *conn, unsigned int passkey);
+void auth_passkey_confirm(struct bt_conn *conn, unsigned int passkey);
+void auth_cancel(struct bt_conn *conn);
+void pairing_complete(struct bt_conn *conn, bool bonded);
+void pairing_failed(struct bt_conn *conn, enum bt_security_err reason);
+extern struct bt_conn_auth_cb conn_auth_callbacks;
+extern struct bt_conn_auth_info_cb conn_auth_info_callbacks;
+#else
+extern struct bt_conn_auth_cb conn_auth_callbacks;
+extern struct bt_conn_auth_info_cb conn_auth_info_callbacks;
+#endif /* CONFIG_BT_NUS_SECURITY_ENABLED */
+
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+void num_comp_reply(bool accept);
+void button_changed(uint32_t button_state, uint32_t has_changed);
+#endif /* CONFIG_BT_NUS_SECURITY_ENABLED */
+
+#endif /* BLE_H */
+
--- src/main.c
+++ src/main.c
@@ -1,19 +1,61 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/** @file
+ *  @brief Nordic UART Bridge Service (NUS) sample
+ */
+#include <uart_async_adapter.h>
+
+#include <zephyr/types.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
+
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
+#include <soc.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+
+#include <bluetooth/services/nus.h>
+
+#include <dk_buttons_and_leds.h>
+
+#include <zephyr/settings/settings.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include <zephyr/logging/log.h>
+
 #include <nrfx_spim.h>
 #include <nrfx_timer.h>
-#include <nrfx_log.h>
 #include <zephyr/sys/atomic.h>
+
+#include "BLE.h"
 #include "spi.h"
 #include "timer.h"
 
+LOG_MODULE_REGISTER(mymain, LOG_LEVEL_DBG);
 static void init_clock();
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected        = connected,
+	.disconnected     = disconnected,
+	.recycled         = recycled_cb,
+#ifdef CONFIG_BT_NUS_SECURITY_ENABLED
+	.security_changed = security_changed,
+#endif
+};
 
-// keeps track of whether stimulation is currently ongoing
-bool stimming = false;
-
-nrfx_err_t status;
+static struct bt_nus_cb nus_cb = {
+	.received = bt_receive_cb,
+};
 
 static void init_misc_pins(void) {
     // Configure P0.16 as output (DAC1 CS)
@@ -34,22 +76,70 @@ static void init_misc_pins(void) {
     nrf_gpio_pin_clear(NRF_GPIO_PIN_MAP(1, 1)); // set low
 }
 
-int main(void){    
+int main(void)
+{
     #if defined(__ZEPHYR__)
         IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_TIMER_INST_GET(TIMER_INST_IDX)), IRQ_PRIO_LOWEST,
                     NRFX_TIMER_INST_HANDLER_GET(TIMER_INST_IDX), 0, 0);
         IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_SPIM_INST_GET(SPIM_INST_IDX)), IRQ_PRIO_LOWEST,
                     NRFX_SPIM_INST_HANDLER_GET(SPIM_INST_IDX), 0, 0);
     #endif
+
     init_clock();
     init_misc_pins();
-    (void)status;
     spi_init();
     timer_init();
     measurement_timer_init();
+	int blink_status = 0;
+	int err = 0;
     uint32_t experiment_counter = 0;
-    while(1) {
-        //NRFX_EXAMPLE_LOG_PROCESS();
+    
+	configure_gpio();
+
+	err = uart_init();
+	if (err) {
+		error();
+	}
+
+	if (IS_ENABLED(CONFIG_BT_NUS_SECURITY_ENABLED)) {
+		err = bt_conn_auth_cb_register(&conn_auth_callbacks);
+		if (err) {
+			LOG_ERR("Failed to register authorization callbacks. (err: %d)", err);
+			return 0;
+		}
+
+		err = bt_conn_auth_info_cb_register(&conn_auth_info_callbacks);
+		if (err) {
+			LOG_ERR("Failed to register authorization info callbacks. (err: %d)", err);
+			return 0;
+		}
+	}
+
+	err = bt_enable(NULL);
+	if (err) {
+		error();
+	}
+
+	LOG_INF("Bluetooth initialized");
+
+	k_sem_give(&ble_init_ok);
+
+	if (IS_ENABLED(CONFIG_SETTINGS)) {
+		settings_load();
+	}
+
+	err = bt_nus_init(&nus_cb);
+	if (err) {
+		LOG_ERR("Failed to initialize UART service (err: %d)", err);
+		return 0;
+	}
+
+	k_work_init(&adv_work, adv_work_handler);
+	advertising_start();
+
+	for (;;) {
+		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
+		//k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
         k_msleep(10000);
         experiment_counter += 10;
         error_data my_error_data;
@@ -65,11 +155,11 @@ int main(void){
                my_error_data.event1_max,
                my_error_data.event2_max,
                my_error_data.event3_max);
-    }
-    return 0;
+	}
 }
 
-
+K_THREAD_DEFINE(ble_write_thread_id, STACKSIZE, ble_write_thread, NULL, NULL,
+		NULL, PRIORITY, 0, 0);
 
 static void init_clock() {
 	// select the clock source: HFINT (high frequency internal oscillator) or HFXO (external 32 MHz crystal)
@@ -80,7 +170,3 @@ static void init_clock() {
     while (NRF_CLOCK_S->EVENTS_HFCLKSTARTED == 0);
     NRF_CLOCK_S->EVENTS_HFCLKSTARTED = 0;
 }
-
-
-
-
